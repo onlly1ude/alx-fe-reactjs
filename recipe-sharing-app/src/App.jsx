23c14b30d2ebbbb956faeff44f67610fa373792f
emmanuel <only1ude@gmail.com>
2024-08-25 19:27:38 -0500

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,45 +1,15 @@
-// App.jsx
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm";
-import RecipeDetails from "./components/RecipeDetails";
-import SearchBar from "./components/SearchBar";
-import FavoritesList from "./components/FavoritesList";
-import RecommendationsList from "./components/RecommendationsList";
+// src/App.jsx
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
-  const [currentRecipe, setCurrentRecipe] = useState(null);
-
-  const handleEdit = (recipe) => {
-    setCurrentRecipe(recipe);
-  };
-
   return (
-    <Router>
-      <div>
-        <h1>Recipe Sharing App</h1>
-        {/* Add the SearchBar component */}
-        <SearchBar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <AddRecipeForm
-                  currentRecipe={currentRecipe}
-                  setCurrentRecipe={setCurrentRecipe}
-                />
-                <RecipeList onEdit={handleEdit} />
-                <FavoritesList />
-                <RecommendationsList />
-              </>
-            }
-          />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
   );
 }
 
